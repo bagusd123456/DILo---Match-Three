f9e40157cb2078bf5859e44a91909c142bb14078
bagusd123456 <41263511+bagusd123456@users.noreply.github.com>
2021-12-28 17:33:14 +0700

    Initial commit

---
+++ Assets/Script/GameflowManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameflowManager : MonoBehaviour
+{
+
+    #region Singleton
+
+    private static GameflowManager _instance = null;
+
+    public static GameflowManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameflowManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: GameFlowManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    [Header("UI")]
+    public UIGameOver GameOverUI;
+
+    public bool IsGameOver { get { return isGameOver; } }
+
+    private bool isGameOver = false;
+
+    private void Start()
+    {
+        isGameOver = false;
+    }
+
+    public void GameOver()
+    {
+        isGameOver = true;
+        ScoreManager.Instance.SetHighScore();
+        GameOverUI.Show();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
